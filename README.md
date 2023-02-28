8974378da10fe23019324e603a5080c67ce89d72
Ibrahim Raimi <raimiibrahim44@gmail.com>
2023-02-28 17:02:47 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Folio 2023
+
+### Credit to: [Bokoko](https://bokoko33.me/)
---
+++ main.js
@@ -0,0 +1,5 @@
+import "./src/style/style.css";
+// import Experience from "./Experience/Experience.js";
+import Experience from "./src/Experience/Experience.js";
+
+const experience = new Experience(document.querySelector(".experience-canvas"));
